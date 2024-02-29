4599827bb48bbdc1b40b27208a9162acee24cdba
Stefano <stefano.turcarelli@icloud.com>
2024-02-29 11:12:37 -0600

    Add project files.

---
+++ EntityFrameworkDemo/Controllers/EmployeeController.cs
@@ -0,0 +1,25 @@
+﻿using EntityFrameworkDemo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EntityFrameworkDemo.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+        public ActionResult Index()
+        {
+            // Create an instance of the Entity Framework context
+            EntityFrameworkDemoEntities entities = new EntityFrameworkDemoEntities();
+
+            // Get the list of employees from the database
+            List<Employee> employees = entities.Employees.ToList();
+
+            // Pass the list of employees to the view
+            return View(employees);
+        }
+    }
+}
